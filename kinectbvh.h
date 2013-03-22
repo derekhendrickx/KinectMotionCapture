93f0c373139cac38b6f953eb608fa20522adec4c
Derek Hendrickx <derek.hendrickx@gmail.com>
2013-03-22 13:40:17 +0100

    Revert "Update kinectbvh.h"
    This reverts commit 2acfd69ca36ed01e71fa66f7a43109581cc7bac6.

--- kinectbvh.h
+++ kinectbvh.h
@@ -23,21 +23,15 @@ public:
 	~KinectBVH();
 
 	void AddOffset(const Vector4);
-	void AddMotionData(std::vector<Vector4> offsets);
-	void AddFramePerSecond(float fps);
-	void AddNbFrames(int nbFrames);
+
 	bool CreateBVHFile(QString);
 	void CreateSkeletonInformation(const NUI_SKELETON_DATA &);
-	void InitMotion();
+
 private:
 	QFile *m_pFile;
 	QString m_vFileName;
 
 	bvh_s *m_pBVH_s;
-	float m_framePerSecond;
-	int m_nbFrame;
-	bool m_initMotion;
-	bool m_initHierarchy;
 };
 
 #endif // KINECTBVH_H
