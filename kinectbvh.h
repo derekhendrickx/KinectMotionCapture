384c24464031c26ec282233e3c36511138aaa0fa
Mak13 <benjamin.makusa@gmail.com>
2013-03-25 11:09:03 +0100

    Update kinectbvh.h
    AddMotionFrame added

--- kinectbvh.h
+++ kinectbvh.h
@@ -25,15 +25,22 @@ public:
 	~KinectBVH();
 
 	void AddOffset(const KinectVector4 &);
-
+	void AddMotionFrame(std::vector<KinectVector4> offsets);
+	void AddFramePerSecond(float fps);
+	void AddNbFrames(int nbFrames);
 	bool CreateBVHFile(QString);
 	void CreateSkeletonInformation(const NUI_SKELETON_DATA &);
-
+	void InitMotion();
+	std::vector<KinectVector4> GetOffset();
 private:
 	QFile *m_pFile;
 	QString m_vFileName;
 
 	bvh_s *m_pBVH_s;
+	float m_framePerSecond;
+	int m_nbFrame;
+	bool m_initMotion;
+	bool m_initHierarchy;
 };
 
 #endif // KINECTBVH_H
